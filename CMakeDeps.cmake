ae718a1bacdde511049325f2ff12e9346d948834
gknowles <gknowles@ieee.org>
2017-01-08 02:07:48 -0800

    Consolidate into single CMakeLists.txt

--- CMakeDeps.cmake
+++ CMakeDeps.cmake
@@ -7,3 +7,15 @@
 # appveyor.yml
 # configure.bat
 # docs/README.md
+# include/dimcli/cli.h
+# include/dimcli/config.h
+# include/dimcli/config_suffix.h
+# include/dimcli/console.h
+# src/argv.cpp
+# src/cli.cpp
+# src/console.cpp
+# src/pch.cpp
+# src/pch.h
+# test/clitest.cpp
+# test/pch.cpp
+# test/pch.h
