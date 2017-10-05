c079efcc5c02f24d6fb8acf3d9a65205a7ef7dcc
Glen Knowles <gknowles@ieee.org>
2017-10-04 17:22:35 -0700

    Update configure.bat

--- configure.bat
+++ configure.bat
@@ -1,7 +1,7 @@
 :: Copyright Glen Knowles 2016 - 2017.
 :: Distributed under the Boost Software License, Version 1.0.
 @echo off
-rd /s/q build
+if exist build rd /s/q build
 md build & cd build
 cmake -G "Visual Studio 15 2017 Win64" ..
 cd ..
