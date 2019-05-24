140b0d0501d166eb6d7f06462ce1152d2ad41748
gknowles <gknowles@ieee.org>
2019-05-24 00:24:37 -0700

    Use vs2019 by default

--- configure.bat
+++ configure.bat
@@ -1,7 +1,7 @@
-:: Copyright Glen Knowles 2016 - 2017.
+:: Copyright Glen Knowles 2016 - 2019.
 :: Distributed under the Boost Software License, Version 1.0.
 @echo off
 if exist build rd /s/q build
 md build & cd build
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 16 2019" -A x64 -T v142 ..
 cd ..
