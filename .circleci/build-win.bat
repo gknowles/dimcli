ba72cfd05c67ff3e748d4742dff40534da8a4abe
gknowles <gknowles@ieee.org>
2025-06-06 08:09:04 -0700

    More CircleCI

--- .circleci/build-win.bat
+++ .circleci/build-win.bat
@@ -1,2 +1,12 @@
 :: Copyright Glen Knowles 2025
 :: Distributed under the Boost Software License, Version 1.0.
+@echo off
+cd %~dp0
+call %p_init%
+cd ..
+md build
+cd build
+cmake .. -G "%p_generator%" -A %p_platform% -T %p_toolset%
+cmake . -DCMAKE_BUILD_TYPE=%p_config% -DMAKE_INSTALL_PREFIX=publish ^
+  -DINSTALL_LIBS:BOOL=ON
+cmake --build . --target install --config %p_config%
