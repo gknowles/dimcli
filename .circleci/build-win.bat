ee9c62bc9da3b499f593d8f595510a1c70d7b5d5
gknowles <gknowles@ieee.org>
2025-06-10 20:55:09 -0700

    Update copyright

--- .circleci/build-win.bat
+++ .circleci/build-win.bat
@@ -1,4 +1,4 @@
-:: Copyright Glen Knowles 2025
+:: Copyright Glen Knowles 2025.
 :: Distributed under the Boost Software License, Version 1.0.
 @echo off
 cd %~dp0
--- tests/perf/pch.h
+++ tests/perf/pch.h
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2016 - 2021.
+// Copyright Glen Knowles 2016 - 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // pch.h - dimcli test perf
