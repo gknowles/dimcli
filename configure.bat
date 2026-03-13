2b8276eafee7d0631a08e055b0d3da6d68772e75
gknowles <gknowles@ieee.org>
2026-03-13 05:01:13 -0700

    Add DLL option to configure.bat

--- configure.bat
+++ configure.bat
@@ -2,25 +2,44 @@
 :: Distributed under the Boost Software License, Version 1.0.
 @echo off
 setlocal
-if "%1" equ "" (
-    set toolset=v145
-) else (
+set toolset=v145
+set args=
+set args2=
+for %%t in (%*) do call :set_arg %%t
+goto :build
+
+:set_arg
+if %1 == v140 (
+    set toolset=%1
+    set args=-DCMAKE_SYSTEM_VERSION="10.0.19041.0"
+)
+if %1 == v140_xp (
+    set toolset=%1
+    set args=-DCMAKE_SYSTEM_VERSION="10.0.19041.0"
+)
+if %1 == v141 (
+    set toolset=%1
+    set args=-DCMAKE_SYSTEM_VERSION="10.0.19041.0"
+)
+if %1 == v142 (
     set toolset=%1
 )
-if "%toolset%" == "v140" goto :sdk_10.19041
-if "%toolset%" == "v140_xp" goto :sdk_10.19041
-if "%toolset%" == "v141" goto :sdk_10.19041
-goto :build
-:sdk_8.1
-set args=-DCMAKE_SYSTEM_VERSION="8.1"
-goto :build
-:sdk_10.19041
-set args=-DCMAKE_SYSTEM_VERSION="10.0.19041.0"
-goto :build
+if %1 == v143 (
+    set toolset=%1
+)
+if %1 == v145 (
+    set toolset=%1
+)
+if /i %1 == DLL (
+    set args2=-DBUILD_SHARED_LIBS:BOOL=ON -DLINK_STATIC_RUNTIME:BOOL=OFF
+)
+goto :eof
 
 :build
 if exist build rd /s/q build
 if exist build goto :eof
 md build & cd build
-cmake .. -G "Visual Studio 18 2026" -A x64 -T %toolset% %args%
+set cmd=cmake .. -G "Visual Studio 18 2026" -A x64 -T %toolset% %args% %args2%
+echo %cmd%
+%cmd%
 cd ..
