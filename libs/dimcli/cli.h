462504dc43c2fc62783acea36af8c1d1792ba372
gknowles <gknowles@ieee.org>
2026-09-14 03:18:40 -0700

    .

--- libs/dimcli/cli.h
+++ libs/dimcli/cli.h
@@ -2465,9 +2465,11 @@ A & Cli::OptShim<A, T>::prompt(int flags) {
 //===========================================================================
 template <typename A, typename T>
 A & Cli::OptShim<A, T>::prompt(const std::string & msg, int flags) {
-    return after([msg, flags](auto & cli, auto & opt, auto & /* val */) {
-        cli.prompt(opt, msg, flags);
-    });
+    return after( // LCOV_EXCL_LINE - gcov refuses to admit this is covered.
+        [msg, flags](auto & cli, auto & opt, auto & /* val */) {
+            cli.prompt(opt, msg, flags);
+        }
+    );
 }
 
 //===========================================================================
