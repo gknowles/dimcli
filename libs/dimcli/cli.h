b94c5a67259058997cf6f66b5dfd19d75fb1ea55
gknowles <gknowles@ieee.org>
2026-09-13 17:37:32 -0700

    .

--- libs/dimcli/cli.h
+++ libs/dimcli/cli.h
@@ -2465,7 +2465,7 @@ A & Cli::OptShim<A, T>::prompt(int flags) {
 //===========================================================================
 template <typename A, typename T>
 A & Cli::OptShim<A, T>::prompt(const std::string & msg, int flags) {
-    return after([msg, flags](auto & cli, auto & opt, auto & /* val */) {
+    return after([msg, flags](auto & cli, auto & opt, auto & val) {
         cli.prompt(opt, msg, flags);
     });
 }
